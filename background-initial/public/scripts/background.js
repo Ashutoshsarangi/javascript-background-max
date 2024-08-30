230e5b211837dc425a538c628631ae38e409141b
sarangia <sarangia@amdocs.com>
2024-08-30 17:10:51 +0300

    sevice worker

--- background-initial/public/scripts/background.js
+++ background-initial/public/scripts/background.js
@@ -1,21 +1,36 @@
-// Page Visibility API
+navigator.serviceWorker.register("/sw.js");
 
+// Page Visibility API
+let backgroundInitialTimeStamp;
+window.addEventListener("visibilitychange", (event) => {
+  if (document.visibilityState === "hidden") {
+    // We are in the background
+    const now = new Date().toLocaleTimeString();
+    backgroundInitialTimeStamp = performance.now();
+    log(`Going to Background at ${now}`);
+  } else {
+    // We are back from the Background
+    const timeElapsed = Math.floor(
+      performance.now() - backgroundInitialTimeStamp
+    );
+    log(`We are back from background after, ${timeElapsed / 1000}s`);
+  }
+});
 
 // Beacon
-document.getElementById("btnBeacon").addEventListener("click", event => {
-});
+document.getElementById("btnBeacon").addEventListener("click", (event) => {});
 
 // Background Sync
-document.getElementById("btnSync").addEventListener("click", async event => {
-
-});
+document
+  .getElementById("btnSync")
+  .addEventListener("click", async (event) => {});
 
 // Background Periodic Sync
-document.getElementById("btnPeriodicSync").addEventListener("click", async event => {
-   
-});
+document
+  .getElementById("btnPeriodicSync")
+  .addEventListener("click", async (event) => {});
 
 // Background Fetch
-document.getElementById("btnFetch").addEventListener("click", async event => {
-   
-});
+document
+  .getElementById("btnFetch")
+  .addEventListener("click", async (event) => {});
